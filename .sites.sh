ff12205eef1bf78c9fa9f3f913b0e3890c550227
lowgel <ryan@artix.localdomain>
2024-05-08 00:36:32 +0900

    added cool sites dmenu script

---
+++ .sites.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+selected_item=$(cat ~/.dotfiles/.coolsites.txt | dmenu -sb '#336600' -fn 'monospace-20' -l 5 -p "cool sites")
+
+if [[ $selected_item == "" ]] then
+	exit
+fi
+librewolf $selected_item
